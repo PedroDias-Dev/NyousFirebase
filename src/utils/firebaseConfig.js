30d647edf3e8075d08478cec3ec3ded5d3b9fc9b
Pedro Dias <pedrofelipe18@hotmail.com.br>
2020-12-09 15:32:27 -0300

    Up

---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAOfgqnbe7y1swM5vu12Sy5ABgKvMN4a08",
+    authDomain: "nyous-9b334.firebaseapp.com",
+    projectId: "nyous-9b334",
+    storageBucket: "nyous-9b334.appspot.com",
+    messagingSenderId: "876340377987",
+    appId: "1:876340377987:web:b08ef707b2facf3ed21eb6"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebaseConfig;
